efe4a66bedd2e91590fbb9b725ea1387df04e0fe
Kujtim Hoxha <kujtimii.h@gmail.com>
2025-09-05 13:26:30 +0200

    feat: expose provider options

--- anthropic/provider_options.go
+++ anthropic/provider_options.go
@@ -1,20 +1,20 @@
 package anthropic
 
-type providerOptions struct {
+type ProviderOptions struct {
 	SendReasoning          *bool                   `json:"send_reasoning,omitempty"`
-	Thinking               *thinkingProviderOption `json:"thinking,omitempty"`
+	Thinking               *ThinkingProviderOption `json:"thinking,omitempty"`
 	DisableParallelToolUse *bool                   `json:"disable_parallel_tool_use,omitempty"`
 }
 
-type thinkingProviderOption struct {
+type ThinkingProviderOption struct {
 	BudgetTokens int64 `json:"budget_tokens"`
 }
 
-type reasoningMetadata struct {
+type ReasoningMetadata struct {
 	Signature    string `json:"signature"`
 	RedactedData string `json:"redacted_data"`
 }
 
-type cacheControlProviderOptions struct {
+type CacheControlProviderOptions struct {
 	Type string `json:"type"`
 }
--- openai/provider_options.go
+++ openai/provider_options.go
@@ -1,21 +1,21 @@
 package openai
 
-type reasoningEffort string
+type ReasoningEffort string
 
 const (
-	reasoningEffortMinimal reasoningEffort = "minimal"
-	reasoningEffortLow     reasoningEffort = "low"
-	reasoningEffortMedium  reasoningEffort = "medium"
-	reasoningEffortHigh    reasoningEffort = "high"
+	ReasoningEffortMinimal ReasoningEffort = "minimal"
+	ReasoningEffortLow     ReasoningEffort = "low"
+	ReasoningEffortMedium  ReasoningEffort = "medium"
+	ReasoningEffortHigh    ReasoningEffort = "high"
 )
 
-type providerOptions struct {
+type ProviderOptions struct {
 	LogitBias           map[string]int64 `json:"logit_bias"`
 	LogProbs            *bool            `json:"log_probes"`
 	TopLogProbs         *int64           `json:"top_log_probs"`
 	ParallelToolCalls   *bool            `json:"parallel_tool_calls"`
 	User                *string          `json:"user"`
-	ReasoningEffort     *reasoningEffort `json:"reasoning_effort"`
+	ReasoningEffort     *ReasoningEffort `json:"reasoning_effort"`
 	MaxCompletionTokens *int64           `json:"max_completion_tokens"`
 	TextVerbosity       *string          `json:"text_verbosity"`
 	Prediction          map[string]any   `json:"prediction"`

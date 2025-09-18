e89eaaff81853ec7d1fa85d5beaacbf8d8f28ade
kujtimiihoxha <kujtimii.h@gmail.com>
2025-09-18 13:11:12 +0200

    wip: initial setup with hoooks

---
+++ openrouter/openrouter.go
@@ -0,0 +1,74 @@
+package openrouter
+
+import (
+	"github.com/charmbracelet/fantasy/ai"
+	"github.com/charmbracelet/fantasy/openai"
+	openaiSDK "github.com/openai/openai-go/v2"
+	"github.com/openai/openai-go/v2/option"
+)
+
+type options struct {
+	openaiOptions []openai.Option
+}
+
+type Option = func(*options)
+
+func prepareCallWithOptions(model ai.LanguageModel, params *openaiSDK.ChatCompletionNewParams, call ai.Call) ([]ai.CallWarning, error) {
+	providerOptions := &ProviderOptions{}
+	if v, ok := call.ProviderOptions[Name]; ok {
+		providerOptions, ok = v.(*ProviderOptions)
+		if !ok {
+			return nil, ai.NewInvalidArgumentError("providerOptions", "openrouter provider options should be *openrouter.ProviderOptions", nil)
+		}
+	}
+	_ = providerOptions
+
+	// HANDLE OPENROUTER call modification here
+
+	return nil, nil
+}
+
+func New(opts ...Option) ai.Provider {
+	providerOptions := options{
+		openaiOptions: []openai.Option{
+			openai.WithHooks(openai.Hooks{
+				PrepareCallWithOptions: prepareCallWithOptions,
+			}),
+		},
+	}
+	for _, o := range opts {
+		o(&providerOptions)
+	}
+	return openai.New(providerOptions.openaiOptions...)
+}
+
+func WithBaseURL(baseURL string) Option {
+	return func(o *options) {
+		o.openaiOptions = append(o.openaiOptions, openai.WithBaseURL(baseURL))
+	}
+}
+
+func WithAPIKey(apiKey string) Option {
+	return func(o *options) {
+		o.openaiOptions = append(o.openaiOptions, openai.WithAPIKey(apiKey))
+	}
+}
+
+func WithName(name string) Option {
+	return func(o *options) {
+		o.openaiOptions = append(o.openaiOptions, openai.WithName(name))
+	}
+}
+
+func WithHeaders(headers map[string]string) Option {
+	return func(o *options) {
+		o.openaiOptions = append(o.openaiOptions, openai.WithHeaders(headers))
+	}
+}
+
+func WithHTTPClient(client option.HTTPClient) Option {
+	return func(o *options) {
+		o.openaiOptions = append(o.openaiOptions, openai.WithHTTPClient(client))
+	}
+}
+

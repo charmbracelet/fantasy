c8711ead48d99318e619b2e0f82cdc8a75fed9e7
Kujtim Hoxha <kujtimii.h@gmail.com>
2025-09-24 12:15:29 +0200

    test: add thinking to anthropic

--- providertests/anthropic_test.go
+++ providertests/anthropic_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/charmbracelet/fantasy/ai"
 	"github.com/charmbracelet/fantasy/anthropic"
+	"github.com/stretchr/testify/require"
 	"gopkg.in/dnaeon/go-vcr.v4/pkg/recorder"
 )
 
@@ -14,6 +15,53 @@ func TestAnthropicCommon(t *testing.T) {
 	testCommon(t, []builderPair{
 		{"claude-sonnet-4", builderAnthropicClaudeSonnet4, nil},
 	})
+
+	opts := ai.ProviderOptions{
+		anthropic.Name: &anthropic.ProviderOptions{
+			Thinking: &anthropic.ThinkingProviderOption{
+				BudgetTokens: 4000,
+			},
+		},
+	}
+	testThinking(t, []builderPair{
+		{"claude-sonnet-4", builderAnthropicClaudeSonnet4, opts},
+	}, testGoogleThinking)
+}
+
+func testAnthropicThinking(t *testing.T, result *ai.AgentResult) {
+	reasoningContentCount := 0
+	signaturesCount := 0
+	// Test if we got the signature
+	for _, step := range result.Steps {
+		for _, msg := range step.Messages {
+			for _, content := range msg.Content {
+				if content.GetType() == ai.ContentTypeReasoning {
+					reasoningContentCount += 1
+					reasoningContent, ok := ai.AsContentType[ai.ReasoningPart](content)
+					if !ok {
+						continue
+					}
+					if len(reasoningContent.ProviderOptions) == 0 {
+						continue
+					}
+
+					anthropicReasoningMetadata, ok := reasoningContent.ProviderOptions[anthropic.Name]
+					if !ok {
+						continue
+					}
+					if reasoningContent.Text != "" {
+						if typed, ok := anthropicReasoningMetadata.(*anthropic.ReasoningOptionMetadata); ok {
+							require.NotEmpty(t, typed.Signature)
+							signaturesCount += 1
+						}
+					}
+				}
+			}
+		}
+	}
+	require.Greater(t, reasoningContentCount, 0)
+	require.Greater(t, signaturesCount, 0)
+	require.Equal(t, reasoningContentCount, signaturesCount)
 }
 
 func builderAnthropicClaudeSonnet4(r *recorder.Recorder) (ai.LanguageModel, error) {
